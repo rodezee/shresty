833d4d7b3b4f149a6f66baf589f3351bfc25b322
rodezee <121246195+rodezee@users.noreply.github.com>
2023-04-24 18:20:51 +0200

    test

--- app/www/examples/uname/index.sh
+++ app/www/examples/uname/index.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-NAME="hello world"
+NAME="uname"
 echo "<html><head>"
 echo "<title>$NAME</title>"
 echo '<meta name="description" content="'$NAME'">'
@@ -7,8 +7,6 @@ echo '<meta name="keywords" content="'$NAME'">'
 echo '<meta http-equiv="Content-type" content="text/html;charset=UTF-8">'
 echo '<meta name="ROBOTS" content="noindex">'
 echo "</head><body><pre>"
-echo "Hello World!"
-echo ""
-echo "date and time: "
-date
+echo "COMMAND uname: "
+uname
 echo "</pre></body></html>"
