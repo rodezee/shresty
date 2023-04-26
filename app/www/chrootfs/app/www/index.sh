d00fc105361be0a95eb4282a894cc845bc9f2597
rodezee <121246195+rodezee@users.noreply.github.com>
2023-04-26 19:27:48 +0200

    test

---
+++ app/www/chrootfs/app/www/index.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+NAME="Welcome"
+BASEDIR=$(dirname "$0")
+
+echo "<html><head>"
+echo "<title>$NAME</title>"
+echo '<meta name="description" content="'$NAME'">'
+echo '<meta name="keywords" content="'$NAME'">'
+echo '<meta http-equiv="Content-type" content="text/html;charset=UTF-8">'
+echo '<meta name="ROBOTS" content="noindex">'
+echo "</head><body>"
+echo "<pre>"
+echo "Welcome to the SHELL Website!"
+echo "</pre>"
+echo "example: "
+for f in $BASEDIR/examples/*; do
+  echo "<p>$f</p>"
+done
+echo "<a href=\"./examples\">examples</a>"
+echo "<p><strong>PING:</strong></p>"
+echo "<pre>"
+ping -c 1 google.com
+echo "</pre>"
+echo "</body>"
+echo "</html>"
