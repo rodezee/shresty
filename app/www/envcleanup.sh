4495da858e47a2ab1f042984f87ca93e584d3985
rodezee <rodezee@github.com>
2023-04-28 17:39:27 +0000

    test

--- app/www/envcleanup.sh
+++ app/www/envcleanup.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-cd environments
+cd $(dirname "$0")
 
+cd environments && \
 for d in */ ; do
     EXPFILE=".exptime$(echo -e  "$d" | sed 's/.$//')"
-    [ $(date +%s) -ge $(cat $EXPFILE) ] && rm -Rf $d $EXPFILE
+    [ $(date +%s) -ge $(cat $EXPFILE) ] && rm -Rf $d $EXPFILE && echo "removed expired env: $d" 
 done
