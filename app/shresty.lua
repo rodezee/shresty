ee8776bd531b2cbba6d90fd0cdd9df288563b45c
rodezee <121246195+rodezee@users.noreply.github.com>
2023-04-28 17:46:56 +0200

    test

--- app/shresty.lua
+++ app/shresty.lua
@@ -67,7 +67,7 @@ function _M.run(command, cid, expires, loggerON)
 
   -- RUN EXPIRE COMMAND
   if loggerON then ngx.say("<br>expires: " .. expires) end
-  local handle1 = io.popen("/usr/sbin/chroot " .. cidenv .. " /bin/sh +m -c \"sleep " .. expires .. " && rm -Rf " .. cidenv .. "\" &", "r")
+  local handle1 = io.popen("/usr/sbin/chroot " .. cidenv .. " /bin/sh +m -c \"sleep " .. expires .. "; rm -Rf " .. cidenv .. "\" &", "r")
   if handle1 == "" or handle1 == nil then
       ngx.status = 404
       return
