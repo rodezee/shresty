1465a8c4710efc14470c8972df00b0fe5d706703
rodezee <121246195+rodezee@users.noreply.github.com>
2023-05-02 17:08:55 +0200

    test

--- app/shresty.lua
+++ app/shresty.lua
@@ -17,39 +17,13 @@ local function str_split(s, sep)
   return res 
 end
 
-function _M.exec(command, username, password, basicauth, cid, exptime, loggerON)
+function _M.exec(command, cid, exptime, loggerON)
   if isempty(command) then command = "echo 'Shresty'" end--; ngx.log(ngx.ALERT, "command: " .. command)
-  if isempty(username) then username = os.getenv("SHRESTY_USER") end
-  if isempty(password) then password = os.getenv("SHRESTY_PASSWORD") end--; ngx.log(ngx.ALERT, "username: " .. username .. "  userpassword: " .. userpassword)
-  if not isempty(basicauth) then
-    local str = string.sub(basicauth, 7)
-    local dec = ngx.decode_base64(str)
-    username = dec:match("(.*):")
-    password = dec:match(":(.*)")
-  end
+  if isempty(cid) then cid = 0 end
+  if isempty(exptime) then exptime = 0 end
   if isempty(loggerON) then loggerON = false end
 
-  -- EXECUTE COMMAND
-  local result, err = _M.run(command, "/app/www/environments/", cid, exptime)
-
-  -- RETURN ERROR
-  if err then
-    if err == "connection refused" then
-      ngx.status = 500
-      ngx.print( "database connection refused, wrong port, host or db specified" )
-    elseif err == "authentication exchange unsuccessful" then
-      ngx.status = 401
-      ngx.header['WWW-Authenticate'] = 'Basic realm="Shresty Authorization", charset="UTF-8"'
-      ngx.print( err )
-    else -- unknown connection error
-      ngx.status = 400
-      ngx.print( err )      
-    end
-  else
-    -- RETURN RESULT
-    ngx.status = 200
-    ngx.say( result["output"] )
-  end
+  _M.run(command, "/app/www/environments/", cid, exptime)
 end
 
 function _M.cycle_cleanup(envdir, loggerON)
