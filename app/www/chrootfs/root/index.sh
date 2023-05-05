40965108473d7dee5eb870b206b35245976df16f
rodezee <121246195+rodezee@users.noreply.github.com>
2023-05-05 16:10:17 +0200

    test

--- app/www/chrootfs/root/index.sh
+++ app/www/chrootfs/root/index.sh
@@ -11,11 +11,12 @@ echo '<meta http-equiv="Content-type" content="text/html;charset=UTF-8">'
 echo '<meta name="ROBOTS" content="noindex">'
 echo "</head><body>"
 echo "<pre>"
-echo "Welcome to the SHELL Website!"
+echo "${NAME} to the SHELL Website!"
+echo "basedir=${BASEDIR}"
 echo "</pre>"
-echo "<p><strong>PING:</strong></p>"
-echo "<pre>"
-ping -c 1 google.com
-echo "</pre>"
+echo "<p><a href="/examples/ping">ping</a></p>"
+echo "<p><a href="/examples/date">date</a></p>"
+echo "<p><a href="/examples/cpuinfo">cpuinfo</a></p>"
+echo "<p><a href="/examples/uname">uname</a></p>"
 echo "</body>"
 echo "</html>"
